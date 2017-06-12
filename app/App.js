91733c734471493468eaf1976ef1e797132b6c51
UHanczar <u.hanczar@gmail.com>
2017-06-12 23:22:13 +0300

    11.Adding_state

--- app/App.js
+++ app/App.js
@@ -8,6 +8,17 @@ import 'style-loader!css-loader!sass-loader!./styles/app.scss';
 
 import TodoApp from 'TodoApp';
 
+import {setSearchText, toggleShowCompleted, addTodo} from './actions/actions';
+import store from './store/configureStore';
+
+store.subscribe(() => {
+  console.log('New State', store.getState());
+});
+
+store.dispatch(addTodo('Clean the yard'));
+store.dispatch(setSearchText('yard'));
+store.dispatch(toggleShowCompleted());
+
 // See from here
 ReactDOM.render(
   <TodoApp />
---
+++ app/store/configureStore.js
@@ -0,0 +1,17 @@
+import { combineReducers, createStore, compose } from 'redux';
+
+import {searchTextReducer, showCompletedReducer, todosReducer} from './../reducers/reducers';
+
+const configure = () => {
+  const reducer = combineReducers({
+    searchText: searchTextReducer,
+    showCompleted: showCompletedReducer,
+    todos: todosReducer
+  });
+
+  const store = createStore(reducer, compose(window.devToolsExtension ? window.devToolsExtension() : f => f));
+
+  return store;
+};
+
+export default configure();
